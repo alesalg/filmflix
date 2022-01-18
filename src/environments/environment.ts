218db183b582462b912c38865d0b34bb0b94f247
alesalg <allesalg@gmail.com>
2022-01-18 09:11:41 -0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-332f3',
+    appId: '1:27254273233:web:4717fe1d5c4d1c903241f0',
+    storageBucket: 'filmflix-332f3.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDpzZ0CgFLZR1JyoZbeYzwEhUwe4bpks5o',
+    authDomain: 'filmflix-332f3.firebaseapp.com',
+    messagingSenderId: '27254273233',
+  },
   production: false
 };
 
